4ee90548b70fbabc3115109dcd73b84a77841a4d
dokdokorea <7dpdud@naver.com>
2018-08-15 01:02:22 +0900

    change home activities to linearView

--- yeyoung/app/src/main/java/com/example/user/coalert_front/Home.java
+++ yeyoung/app/src/main/java/com/example/user/coalert_front/Home.java
@@ -1,6 +1,5 @@
 package com.example.user.coalert_front;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -35,7 +34,7 @@ public class Home extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.homepage);
+        setContentView(R.layout.activity_home);
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
