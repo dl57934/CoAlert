76ed94b7f260f71fa52d03991684ed2279d39285
yeyoung_desktop <7dpdud@naver.com>
2018-08-19 19:11:04 +0900

    delete sh_System file

--- sh_System/crawler/img/3/RotationImg.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-img = cv2.imread('0.jpg')
-rows, cols = img.shape[0:2]
-
-for angle in range(1, 25):
-    M = cv2.getRotationMatrix2D((rows/2, cols/2), angle*15, 1)
-    dst = cv2.warpAffine(img, M, (cols, rows))
-    cv2.imwrite('0'+str(angle*15)+'.jpg', dst)
