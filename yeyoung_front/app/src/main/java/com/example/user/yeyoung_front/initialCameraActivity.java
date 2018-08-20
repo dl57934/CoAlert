e533d712b3e1012e351637fccbfa3acaeb5f8b3a
yeyoung_desktop <7dpdud@naver.com>
2018-08-20 12:48:17 +0900

    activiate login button and change search bar layout

---
+++ yeyoung_front/app/src/main/java/com/example/user/yeyoung_front/initialCameraActivity.java
@@ -0,0 +1,16 @@
+package com.example.user.yeyoung_front;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class initialCameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_initial_camera);
+
+    }
+
+
+}
